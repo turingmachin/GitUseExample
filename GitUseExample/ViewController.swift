e7949279c22c04168344c883b1783ae190890c25
Jared Qu <phantoml0rdplays@gmail.com>
2020-10-06 11:34:59 -0500

    Initial Commit

---
+++ GitUseExample/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  GitUseExample
+//
+//  Created by Richard Castagna on 11/16/18.
+//  Copyright © 2018 Richard Castagna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func buttonClicked(_ sender: UIButton) {
+        print("You finished!")
+    }
+
+}
+
